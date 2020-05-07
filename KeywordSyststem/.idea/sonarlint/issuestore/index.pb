ebac3f480ecbc114a63b0b8b154019023711fc8d
Chukaidong <17302010007@fudan.edu.cn>
2020-05-07 23:25:38 +0800

    set author ... 7

---
+++ KeywordSyststem/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+H
+src/main/java/Start.java,a\0\a0064be54bf53fa905ba704774cb32bd372a9f6d
+W
+'src/main/java/config/LoginKeyWords.java,a\5\a526fada3c35bca5e1afc456253f877119f1b8a4
--- KeywordSyststem/src/main/java/config/SetQuestionKeyWords.java
+++ KeywordSyststem/src/main/java/config/SetQuestionKeyWords.java
@@ -4,12 +4,51 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import startEngine.ExcelEngine;
 import utility.Constants;
 
 //"出题"相关的关键字实现
 public class SetQuestionKeyWords {
     public static WebDriver driver=null;
+
+
+
+    //章节、知识点、作者、评审、质管、题目类型、题目语言
+    public static void SetChapter(){
+        Select chapterList = new Select(driver.findElement(By.id("chapter-select")));
+        chapterList.selectByIndex(0);
+    }
+
+    public static void SetKnowledgePoint() {
+        Select knowledgeList = new Select(driver.findElement(By.id("knowledge-point-select")));
+        knowledgeList.selectByIndex(0);
+    }
+
+    public static void SetAuthor(){
+        Select authorList = new Select(driver.findElement(By.id("author-list")));
+        authorList.selectByIndex(1);
+    }
+
+    public static void SetReviewer(){
+        Select reviewerList = new Select(driver.findElement(By.id("reviewer-list")));
+        reviewerList.selectByIndex(2);
+    }
+
+    public static void SetQuality(){
+        Select qualityList = new Select(driver.findElement(By.id("question-qa-list")));
+        qualityList.selectByIndex(3);
+    }
+
+    public static void SetType(){
+        Select typeList = new Select(driver.findElement(By.id("question-type-list")));
+        typeList.selectByIndex(0);
+    }
+
+    public static void SetLanguage(){
+        Select languageList = new Select(driver.findElement(By.id("question-language-list")));
+        languageList.selectByIndex(0);
+    }
 
     public static void Set_AuthorStartDate() {
         driver.findElement(By.id("authorStartDate")).sendKeys(Constants.authorStartDate);
